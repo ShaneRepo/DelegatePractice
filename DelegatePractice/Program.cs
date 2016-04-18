05d916da74e11c52003cd35a6d9654892b176bb6
Shane <shane8641@live.com>
2016-04-18 06:43:35 -0400

    added basic example implementation

--- DelegatePractice/Program.cs
+++ DelegatePractice/Program.cs
@@ -8,9 +8,24 @@ namespace DelegatePractice
 {
     class Program
     {
+        public delegate string Example();
+
+        public string ExampleIml()
+        {
+            return "Hi";
+        }
+
+        public void DoStuff()
+        {
+            Example myImp;
+            myImp = ExampleIml;
+            Console.WriteLine(myImp());
+        }
         static void Main(string[] args)
         {
-
+            Program p = new Program();
+            p.DoStuff();
+            Console.ReadKey();
         }
     }
 }
