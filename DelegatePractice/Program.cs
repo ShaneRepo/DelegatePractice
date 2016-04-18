3f1b8b05d2838da8b4d239a23c078cd82a04ceae
Shane <shane8641@live.com>
2016-04-18 07:31:02 -0400

    added delegate to accept string name

--- DelegatePractice/Program.cs
+++ DelegatePractice/Program.cs
@@ -8,18 +8,28 @@ namespace DelegatePractice
 {
     class Program
     {
+        // delegate signatures
         public delegate string Example();
-
+        public delegate string ExampleWithString(string name);
+        // Example Implementation Method
         public string ExampleIml()
         {
             return "Hi";
         }
-
+        // Overloaded with string param
+        public string ExampleIml(string name)
+        {
+            return $"Hi {name}";
+        }
+        // Method to assign delegate a method and print to console
         public void DoStuff()
         {
             Example myImp;
+            ExampleWithString myImpString;
             myImp = ExampleIml;
+            myImpString = ExampleIml;
             Console.WriteLine(myImp());
+            Console.WriteLine(myImpString("Shane"));
         }
         static void Main(string[] args)
         {
